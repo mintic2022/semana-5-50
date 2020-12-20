a6b92f8fa007c7203aeac4eb857ba8a8138f94c7
fasadif <fasadif@yahoo.com>
2020-12-20 15:27:47 -0500

    Issue6 Add imagen a los Articulos y metodos actualizados

--- controllers/articuloController.js
+++ controllers/articuloController.js
@@ -16,8 +16,8 @@ exports.list = async(req, res, next) =>{
 
 exports.add = async (req, res, next) =>{
     try {
-        const catExiste = await db.Articulo.findOne({where: {nombre: req.body.nombre}}) //verificoi si la Articulo ya existe antes de crearla
-        if (!catExiste){
+        const elemento = await db.Articulo.findOne({where: {nombre: req.body.nombre}}) //verificoi si la Articulo ya existe antes de crearla
+        if (!elemento){
             const registro = await db.Articulo.create(req.body);
             res.status(200).json(registro); //requerimiento
         }
@@ -34,12 +34,18 @@ exports.add = async (req, res, next) =>{
 
 exports.update = async(req, res, next) =>{
     try {
-        const register = await db.Articulo.update({codigo: req.body.codigo, nombre: req.body.nombre, descripcion: req.body.descripcion},
-            {
-                where: {
-                    id: req.body.id
-                }
-            });
+        const register = await db.Articulo.update({
+            codigo: req.body.codigo,
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            categoriaId: req.body.categoriaId,
+            imagen: req.body.imagen
+        },
+        {
+            where: {
+                id: req.body.id
+            }
+        });
             res.status(200).json(register); //requerimiento
     } catch (error) {
         res.status(500);
--- models/articulo.js
+++ models/articulo.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo( models.Categoria, { foreignKey: 'categoriaId', as: 'categoria'})
     }
   };
   Articulo.init({
@@ -18,7 +18,8 @@ module.exports = (sequelize, DataTypes) => {
     nombre: DataTypes.STRING,
     descripcion: DataTypes.STRING,
     estado: DataTypes.INTEGER,
-    categoriaId: DataTypes.INTEGER
+    categoriaId: DataTypes.INTEGER,
+    imagen: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Articulo',
